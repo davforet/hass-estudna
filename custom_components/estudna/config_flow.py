5d1da56dd494c808d522c34adf13ab4288aadacd
davforet <61906650+davforet@users.noreply.github.com>
2023-08-24 09:21:08 +0200

    Update config_flow.py

--- custom_components/estudna/config_flow.py
+++ custom_components/estudna/config_flow.py
@@ -65,7 +65,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             _LOGGER.exception("Unexpected exception")
             errors["base"] = "unknown"
         else:
-            return self.async_create_entry(title="eSTUDNA", data=user_input)
+            return self.async_create_entry(title="eSTUDNA SEA", data=user_input)
 
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
